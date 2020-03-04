a5191112ad204b36009c8289df98a5ca8ba5fbf7
Alexander Ryzhikov <coobaha@gmail.com>
2020-03-04 10:45:59 +0200

    Add bucklescript 7.2.0 support

--- identify.js
+++ identify.js
@@ -2,14 +2,24 @@
 
 var VERSION = require("./package.json").version,
    ARCH = require("arch")(),
-   cp = require("child_process")
+   cp = require("child_process"),
+   path = require("path");
 
 var bsc_version_regex = /(\w+)\s+(\d+\.\d+\.\d+)(-[^\s]+)?\s+\(\s*Using\s+OCaml:?(\d+\.\d+\.\d+)\+BS\s*\)/i
 
+var bsc_executable;
+
 // NOTE: This doesn't invoke a shell, and thus it doesn't consider $PATH. Intentionally.
 // I'd rather be subject to the vagaries of *just* npm, instead of both npm and the shell.
-var bsc_executable = require.resolve("bs-platform/lib/bsc.exe"),
-   bsc_version = cp.execFileSync(bsc_executable, ["-version"], { encoding: "utf8" }),
+// First tries to resolve >= 7.2.0 location
+// On fail fallbacks to < 7.2.0 location
+try {
+	bsc_executable = require.resolve(path.join("bs-platform", process.platform, "bsc.exe"));
+} catch (e) {
+	bsc_executable = require.resolve("bs-platform/lib/bsc.exe");
+}
+
+var bsc_version = cp.execFileSync(bsc_executable, ["-version"], { encoding: "utf8" }),
    mr = bsc_version.match(bsc_version_regex)
 
 if (null == mr || null == mr[4]) {
