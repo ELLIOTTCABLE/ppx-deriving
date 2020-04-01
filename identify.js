c64a9c6bad8df0b4eda8f00d9232674607bda841
Eric S Bullington <eric.bullington@ninefx.com>
2020-04-01 19:30:34 -0400

    Remove comment that is no longer applicable

--- identify.js
+++ identify.js
@@ -6,8 +6,6 @@ var VERSION = require("./package.json").version,
 
 var bsc_version_regex = /(\w+)\s+(\d+\.\d+\.\d+)(-[^\s]+)?\s+\(\s*Using\s+OCaml:?(\d+\.\d+\.\d+)\+BS\s*\)/i
 
-// NOTE: This doesn't invoke a shell, and thus it doesn't consider $PATH. Intentionally.
-// I'd rather be subject to the vagaries of *just* npm, instead of both npm and the shell.
 var bsc_executable = require.resolve("bs-platform/bsc"),
    bsc_version = cp.execFileSync(bsc_executable, ["-version"], { encoding: "utf8" }),
    mr = bsc_version.match(bsc_version_regex)
